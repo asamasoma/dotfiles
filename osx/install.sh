bef174d6c5dbb11eec8385ae2b234c9955be0119
Amasa Amos <amasa.amos@intentmedia.com>
2020-06-23 10:53:09 -0400

    AA: Disable osx customization

--- osx/install.sh
+++ osx/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/enb bash
+#!/usr/bin/env bash
 
 # The Brewfile handles Homebrew-based app and library installs, but there may
 # still be updates and installables in the Mac App Store. There's a nifty
