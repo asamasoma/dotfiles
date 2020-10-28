746fdb6ebec7a3303a2d8223670be8567d0819e1
Amasa Amos <asamasoma@intentmedia.com>
2020-10-28 13:00:05 -0400

    Tidy up git aliases

--- git/.zsh/git.zsh
+++ git/.zsh/git.zsh
@@ -3,8 +3,7 @@ alias gs='git status -sb'
 alias gx='gitx --all'
 alias ghist='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 alias git-cleanup="git branch --merged | grep -v '^* master$' | grep -v '^  master$' | xargs git branch -d && git remote prune origin"
-alias grpush='git push origin $(git_current_branch):remote-run/$(git_current_branch)'
-alias gfrpush='git push -f origin $(git_current_branch):remote-run/$(git_current_branch)'
+alias gfpush='git push -f origin $(git_current_branch):$(git_current_branch)'
 
 ### Functions
 
