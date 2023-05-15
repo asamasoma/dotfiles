c4e65fe53f2c30d3aadeb36af2356b07166de8b5
Amasa Amos <asamasoma@gmail.com>
2023-05-15 12:36:45 -0400

    Update nvm completion path

--- node/.zsh/env/nvm.zsh
+++ node/.zsh/env/nvm.zsh
@@ -2,5 +2,5 @@ export NODE_MODULES=./node_modules/.bin
 export PATH=$PATH:$NODE_MODULES
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
