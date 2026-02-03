41c7b2b09805659d4d5e629ae4e79bc8be0734d6
Amasa Amos <asamasoma@gmail.com>
2026-02-03 12:00:53 -0500

    Add gcan alias to git aliases

--- git/.zsh/git.zsh
+++ git/.zsh/git.zsh
@@ -4,6 +4,7 @@ alias gx='gitx --all'
 alias ghist='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
 alias git-cleanup="git branch --merged | grep -v '^* master$' | grep -v '^  master$' | xargs git branch -d && git remote prune origin"
 alias gfpush='git push -f origin $(git_current_branch):$(git_current_branch)'
+alias gcan='git commit --amend --no-edit'
 
 ### Functions
 
